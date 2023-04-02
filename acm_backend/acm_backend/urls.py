5c114bb8a5aadb0456938d2a8b05e38e5c4087cc
root <pkverma131@gmail.com>
2023-04-02 07:24:15 +0000

    media root if debug

--- acm_backend/acm_backend/urls.py
+++ acm_backend/acm_backend/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('catalogue/', include('catalogue.urls'))
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
