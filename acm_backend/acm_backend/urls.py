bd5b3068b12624ab9a4de1f447db2696957e11da
root <pkverma131@gmail.com>
2023-04-02 10:11:25 +0000

    index app added

--- acm_backend/acm_backend/urls.py
+++ acm_backend/acm_backend/urls.py
@@ -17,12 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
-
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('', include('index.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('catalogue/', include('catalogue.urls'))
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
