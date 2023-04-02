a135938b1adf4b12d66c86171aa4e2a6af5c3767
root <pkverma131@gmail.com>
2023-04-02 08:58:01 +0000

    index page added

--- acm_backend/acm_backend/urls.py
+++ acm_backend/acm_backend/urls.py
@@ -17,14 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework import routers
-from .views import index
+from . import views
 
-router = routers.DefaultRouter()
-router.register(r'', index)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('catalogue/', include('catalogue.urls'))
