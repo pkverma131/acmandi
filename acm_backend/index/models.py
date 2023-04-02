ed36784bc48f9df3242aac21ef6d12665e165a82
root <pkverma131@gmail.com>
2023-04-02 17:16:59 +0000

    model object display string added

--- acm_backend/index/models.py
+++ acm_backend/index/models.py
@@ -6,10 +6,16 @@ class TileFilter(AuditModelMixin):
     filter_key = models.CharField(max_length=20)
     filter_value = models.CharField(max_length=20)
 
+    def __str__(self):
+        return f'key {self.filter_key} value {self.filter_value}'
+
 
 class TileCategory(AuditModelMixin):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
     def tile(self):
         return Tile.objects.filter(tile_category=self.id).values()
@@ -21,6 +27,9 @@ class Tile(AuditModelMixin):
     description = models.CharField(max_length=200)
     thumbnail = models.ImageField(upload_to='tiles/thumbnail/', default=None)
 
+    def __str__(self):
+        return self.name
+
     def filter(self):
         return TileToTileFilter.objects.filter(tile=self.id).values()
 
@@ -28,3 +37,4 @@ class Tile(AuditModelMixin):
 class TileToTileFilter(models.Model):
     tile = models.ForeignKey(Tile, on_delete=models.CASCADE)
     tile_filter = models.ForeignKey(TileFilter, on_delete=models.CASCADE)
+
