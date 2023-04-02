231664aa7ebfbbc06cc1ca05c1cec310966ca1c7
root <pkverma131@gmail.com>
2023-04-02 16:44:42 +0000

    Home page tiles related code changes in views, models and serializers

--- acm_backend/index/models.py
+++ acm_backend/index/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from catalogue.models import AuditModelMixin
 
-# Create your models here.
+
+class TileFilter(AuditModelMixin):
+    filter_key = models.CharField(max_length=20)
+    filter_value = models.CharField(max_length=20)
+
+
+class TileCategory(AuditModelMixin):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+
+    def tile(self):
+        return Tile.objects.filter(tile_category=self.id).values()
+
+
+class Tile(AuditModelMixin):
+    tile_category = models.ForeignKey(TileCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    thumbnail = models.ImageField(upload_to='tiles/thumbnail/')
+
+    def filter(self):
+        return TileToTileFilter.objects.filter(tile=self.id).values()
+
+
+class TileToTileFilter(AuditModelMixin):
+    tile = models.ForeignKey(Tile, on_delete=models.CASCADE)
+    tile_filter = models.ForeignKey(TileFilter, on_delete=models.CASCADE)
--- acm_backend/index/views.py
+++ acm_backend/index/views.py
@@ -1,11 +1,21 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import TileCategory
+from .serializers import TileSerializer
 
 
 def index(request):
     """View function for home page of site."""
 
+    # TODO: Add tiles category
+    # TODO: Add tiles to each category
+    # TODO: Each tiles will have title, image and filter data
+
     template = loader.get_template('acmandi/index.html')
+
+    queryset = TileCategory.objects.all()
+    serializer_class = TileSerializer(queryset, many=True)
+    serialized_data = {'data': serializer_class.data}
 
     context = {
         'products': ['Daikin 1.5 Ton 5 Star Inverter Split AC - FTKG50TV',
@@ -17,6 +27,7 @@ def index(request):
                      'Carrier 1.5 Ton 5 Star Inverter Split AC - CAI18EK5R39F0',
                      'Godrej 1.5 Ton 5 Star Inverter Split AC - GIC 18HTC5-WTA',
                      'Whirlpool 1.5 Ton 5 Star Inverter Split AC - 3D Cool Elite Pro',
-                     'Mitsubishi Heavy Duty 1.5 Ton 5 Star Inverter Split AC - SRK20CSS-S6']
+                     'Mitsubishi Heavy Duty 1.5 Ton 5 Star Inverter Split AC - SRK20CSS-S6'],
+        'tiles': serialized_data
     }
     return HttpResponse(template.render(context, request))
