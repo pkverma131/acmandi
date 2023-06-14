caaed002dc37a3f31c12a6afcf9fa814bb5b3aaf
root <pkverma131@gmail.com>
2023-06-14 09:47:10 +0000

    image manupulated and alignment change

--- acm_frontend/src/App.js
+++ acm_frontend/src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 
 var path = process.env.PUBLIC_URL;
 
-const Tile = ({ title, image }) => {
+const Tile = ({ title, description, image }) => {
   return (
     <div className="tile">
       <img src={image} alt={title} />
+      <p className="tile-description">{description}</p>
+      <h3 className="tile-title">{title}</h3>
     </div>
   );
 };
@@ -18,7 +20,7 @@ const Category = ({ title, tiles }) => {
       <h2>{title}</h2>
       <div className="tile-container">
         {tiles.map((tile, index) => (
-          <Tile key={index} title={tile.title} image={path +tile.image} />
+          <Tile key={index} title={tile.title} description={tile.description} image={path +tile.image} />
         ))}
       </div>
     </div>
@@ -31,18 +33,18 @@ const HomePage = () => {
     {
       title: 'Size Up Your Room',
       tiles: [
-        { title: 'Small Bedroom', image:  '/media/tiles/thumbnail/small_bedroom.png' },
-        { title: 'Big Bedroom', image: '/media/tiles/thumbnail/big_bedroom.png' },
-        { title: 'Living Room', image: '/media/tiles/thumbnail/living_room.png' }
+        { title: 'Small Bedroom', description:'120-170 sq.ft.', image:  '/media/tiles/thumbnail/small_bedroom.png' },
+        { title: 'Big Bedroom',description:'120-170 sq.ft.',  image: '/media/tiles/thumbnail/big_bedroom.png' },
+        { title: 'Living Room', description:'120-170 sq.ft.', image: '/media/tiles/thumbnail/living_room.png' }
       ]
     },
     {
       title: 'Choose Your Type',
       tiles: [
-        { title: 'Split AC', image: '/media/tiles/thumbnail/split_ac.png' },
-        { title: 'Window AC', image: '/media/tiles/thumbnail/window_ac.png' },
-        { title: 'Portable AC', image: '/media/tiles/thumbnail/portable_ac.png' },
-        { title: 'Hot and Cold AC', image: '/media/tiles/thumbnail/hotncold_ac.png' }
+        { title: 'Split AC', description:'120-170 sq.ft.', image: '/media/tiles/thumbnail/split_ac.png' },
+        { title: 'Window AC', description:'120-170 sq.ft.', image: '/media/tiles/thumbnail/window_ac.png' },
+        { title: 'Portable AC', description:'120-170 sq.ft.', image: '/media/tiles/thumbnail/portable_ac.png' },
+        { title: 'Hot and Cold AC', description:'120-170 sq.ft.', image: '/media/tiles/thumbnail/hotncold_ac.png' }
       ]
     }
   ];
