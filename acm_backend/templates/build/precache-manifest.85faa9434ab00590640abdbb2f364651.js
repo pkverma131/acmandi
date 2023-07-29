5a0c96df639b67341448a023b6280dacdcc2cc49
root <pkverma131@gmail.com>
2023-07-29 07:16:46 +0000

    build added

---
+++ acm_backend/templates/build/precache-manifest.85faa9434ab00590640abdbb2f364651.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c998fc3099e7c7457b71",
+    "url": "http://localhost:3000/public/media/tiles/static/css/main.0c226752.chunk.css"
+  },
+  {
+    "revision": "c998fc3099e7c7457b71",
+    "url": "http://localhost:3000/public/media/tiles/static/js/main.f682c8fc.chunk.js"
+  },
+  {
+    "revision": "c96e26e0141e71b826cc",
+    "url": "http://localhost:3000/public/media/tiles/static/js/runtime~main.4b9cc936.js"
+  },
+  {
+    "revision": "cc652979ae51b9d86b34",
+    "url": "http://localhost:3000/public/media/tiles/static/css/2.7a6fbdfd.chunk.css"
+  },
+  {
+    "revision": "cc652979ae51b9d86b34",
+    "url": "http://localhost:3000/public/media/tiles/static/js/2.ce68e7c5.chunk.js"
+  },
+  {
+    "revision": "0b047dc5cc4f2a268bc55c197e66db1b",
+    "url": "http://localhost:3000/public/media/tiles/index.html"
+  }
+];
